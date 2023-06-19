6153b586f35da3eb7ea6dff013c63dd819cc835b
Nicolás Valor <nicoo.valor@gmail.com>
2023-06-19 15:02:21 -0300

    Server ready to run

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require("./src/server");
+
+server.listen(3003, () => {
+    console.log("Server running on port: 3003");
+})
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const api = express();
+
+module.exports = api;
