314ada3046ee655858ad8c8fbdfe735b9933e5ca
Nicolás Valor <nicoo.valor@gmail.com>
2023-06-23 16:12:34 -0300

    Setting up prisma

---
+++ prisma/index.js
@@ -0,0 +1,15 @@
+const prisma = new PrismaClient()
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (error) => {
+        console.error(error)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
